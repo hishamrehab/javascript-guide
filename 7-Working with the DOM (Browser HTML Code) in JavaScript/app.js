a741dba7b50ccdac795f65ceb76ec0fa84e8a73d
hishamrehab <hishamrehab30@gmail.com>
2025-11-26 17:23:53 +0200

    Selecting Multiple Elements

--- 7-Working with the DOM (Browser HTML Code) in JavaScript/app.js
+++ 7-Working with the DOM (Browser HTML Code) in JavaScript/app.js
@@ -1,9 +1,36 @@
-// Attributes vs Properties
+// // Attributes vs Properties
 
-const input = document.querySelector("input");
+// const input = document.querySelector("input");
 
-console.dir(input);
+// console.dir(input);
 
 
-// input.value = "new value";
-// input.value = "new value";
+// // input.value = "new value";
+// // input.value = "new value";
+
+
+// const allLis = document.querySelectorAll("li");
+
+// console.dir( "allLis", allLis);
+
+// const listItemElements =  document.querySelectorAll("li");
+const h1 = document.getElementById("main-title");
+
+h1.textContent = "Some new title!";
+h1.style.color = "white";
+h1.style.backgroundColor ="balck";
+
+
+ const li = document.querySelector("li:last-of-type");
+  li.textContent = li.textContent + "(Changed!)";
+ 
+  const body = document.body;
+//   body.querySelector
+
+
+ const listItemElements = document.getElementsByTagName("li");
+ 
+
+for(const listItemEl of listItemElements) {
+    console.dir(listItemEl)
+} 
