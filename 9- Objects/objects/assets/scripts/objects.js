31240216b98e58da3d40705f902e3de44cf35720
hishamrehab <hishamrehab30@gmail.com>
2025-12-08 13:57:47 +0200

    Setting dynamic properties into the object

--- 9- Objects/objects/assets/scripts/objects.js
+++ 9- Objects/objects/assets/scripts/objects.js
@@ -1,20 +1,41 @@
-const person = {
-    name: "Max",
-    age: 30,
-    hobbies: ["Sports" , "Cooking"],
-    great: function() {
-        alert("Hi there");
-    }
-};
+const movieList = document.getElementById("movie-list");
 
-//...
+movieList.style['background-color'] = "red";
+movieList.style.display = "block";
 
-// person.age = 31;
-//  delete person.age;
-// person.age = undefined;
+ const userChosenKeyName = "level";
 
- person.age = null;
- person.isAdmin = true;
 
-console.log(person);
 
+
+    let person = {
+    'first-name': "Max",
+        age: 30,
+        hobbies: ["Sports" , "Cooking"],
+        [userChosenKeyName]: "...",
+        great: function() {
+            alert("Hi there");
+        },
+        1.5 : "hello"
+    };
+
+
+    //...
+
+    // person.age = 31;
+    delete person.age;
+    // person.age = undefined;
+
+    person.age = null;
+    person.isAdmin = true;
+    const keyName = "first name";
+
+
+
+    console.log(person[keyName]);
+    console.log(person[age]);
+    console.log(person[0]);
+    console.log(person["first-name"]);
+    console.log(person.hobbies[1]);
+    console.log(person.great());
+    console.log(person[1.5]);
