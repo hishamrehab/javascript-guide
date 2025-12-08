c9529411b0084c3af353273debde2464887926eb
hishamrehab <hishamrehab30@gmail.com>
2025-12-08 14:20:35 +0200

    Demo App

--- 9- Objects/objects/assets/scripts/objects.js
+++ 9- Objects/objects/assets/scripts/objects.js
@@ -1,41 +1,34 @@
-const movieList = document.getElementById("movie-list");
+const addMovieBtn = document.getElementById("add-movie-btn");
+const searchBtn = document.getElementById("search-btn");
 
-movieList.style['background-color'] = "red";
-movieList.style.display = "block";
-
- const userChosenKeyName = "level";
+const movies = [];
 
 
+const addMovieHandler = () => {
+   const title = document.getElementById("title").value;
+   const extraName = document.getElementById("extra-name").value;
+   const extraValue = document.getElementById("extra-value").value;
+
+ if(title.trim() === "" ||
+  extraName.trim() === "" ||
+   extraValue.trim() === ""
+) {
+   return;
+ }
+  
+
+ const newMovie = {
+   info: {
+     title,
+     [extraName]: extraValue,
+   },
+   id: Math.random().toString(),
+ }
+
+ movies.push(newMovie);
+ console.log(newMovie);   
+};
 
 
-    let person = {
-    'first-name': "Max",
-        age: 30,
-        hobbies: ["Sports" , "Cooking"],
-        [userChosenKeyName]: "...",
-        great: function() {
-            alert("Hi there");
-        },
-        1.5 : "hello"
-    };
+addMovieBtn.addEventListener("click" , addMovieHandler);
 
-
-    //...
-
-    // person.age = 31;
-    delete person.age;
-    // person.age = undefined;
-
-    person.age = null;
-    person.isAdmin = true;
-    const keyName = "first name";
-
-
-
-    console.log(person[keyName]);
-    console.log(person[age]);
-    console.log(person[0]);
-    console.log(person["first-name"]);
-    console.log(person.hobbies[1]);
-    console.log(person.great());
-    console.log(person[1.5]);
